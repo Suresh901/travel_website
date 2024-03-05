d3d8b1b11df260e7323dda8ffa9dfe3cc4062c7a
Suresh901 <sureshshrestha915@gmail.com>
2024-03-05 20:27:14 +0545

    pages added

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 import HeroSection from "./components/HeroSection"
+import Availability from "./components/availability/Availability"
 import CategoryPage from "./components/category/CategoryPage"
-
+import Offering from "./components/offering/Offering"
+import WelcomePage from "./components/welcome/WelcomePage"
 
 const page = () => {
   return (
     <div>
       <HeroSection />
       <CategoryPage />
+      <WelcomePage />
+      <Offering />
+      <Availability />
     </div>
   )
 }
