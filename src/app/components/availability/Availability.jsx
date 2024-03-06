1fecdb5e6df31c894d567ab03e6b478884da510d
Suresh901 <sureshshrestha915@gmail.com>
2024-03-06 11:46:36 +0545

    Accordion added and changes in availability section

--- src/app/components/availability/Availability.jsx
+++ src/app/components/availability/Availability.jsx
@@ -1,17 +1,26 @@
 "use client";
 
+import Accordion from "./components/Accordion";
+import { IoMdPlay } from "react-icons/io";
+
 const Availability = () => {
   return (
-    <div className="w-[90vw] h-[500px] mx-auto mt-[-90px] flex p-10">
-      <div className="w-1/2 ">
+    <div className="w-[90vw] h-auto mx-auto mt-[-90px] flex p-10">
+      <div className="w-1/2 relative">
         <img
           src="https://gaviaspreview.com/wp/gowilds/wp-content/uploads/2023/01/tour-2.jpg"
           alt=""
           className="w-full h-full rounded-tl-md rounded-bl-md"
         />
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <IoMdPlay
+            className="bg-brand rounded-full flex items-center text-white hover:animate-ping p-4 ml-1 cursor-pointer"
+            size={70}
+          />
+        </div>
       </div>
       <div className="w-1/2 bg-[#f3f8f6] rounded-tr-md rounded-br-md">
-        <div className="flex- flex-col items-center p-5 pt-10">
+        <div className="flex flex-col items-start p-5 pt-10 ml-5">
           <h1 className="text-[#63AB45] bg-[#E1EFE0] p-2 rounded-md w-[100px]">
             Availability
           </h1>
@@ -19,7 +28,9 @@ const Availability = () => {
             Explore Real Adventure
           </h1>
         </div>
-        <div></div>
+        <div>
+          <Accordion />
+        </div>
       </div>
     </div>
   );
---
+++ src/app/components/availability/components/Accordion.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { MdKeyboardArrowDown } from "react-icons/md";
+
+const Accordion = () => {
+  const [activeItem, setActiveItem] = useState(null);
+  const [activeBorder, setActiveBorder] = useState(null);
+
+  const toggleItem = (index) => {
+    setActiveItem((prevActiveItem) =>
+      prevActiveItem === index ? null : index
+    );
+  };
+  const toggleBorder = (index) => {
+    setActiveBorder(activeBorder === index ? null : index);
+  };
+  const handleClick = (index) => {
+    toggleItem(index);
+    toggleBorder(index);
+  };
+
+  const faqData = [
+    {
+      question: "How Much Price About Tour & Travels",
+      answer:
+        "Sed rhoncus facilisis purus, at accumsan purus sagittis vitae. Nullam acelit at eros imperdiet pulvinar velut nisl sit placerat neque amet sapien semper tempus.",
+    },
+    {
+      question: "We’re giving all the best services to you",
+      answer:
+        "Sed rhoncus facilisis purus, at accumsan purus sagittis vitae. Nullam acelit at eros imperdiet pulvinar velut nisl sit placerat neque amet sapien semper tempus.",
+    },
+    {
+      question: "Best Experience Travel Agency",
+      answer:
+        "Sed rhoncus facilisis purus, at accumsan purus sagittis vitae. Nullam acelit at eros imperdiet pulvinar velut nisl sit placerat neque amet sapien semper tempus.",
+    },
+  ];
+
+  return (
+    <div className="w-full">
+      <div className="px-10">
+        <div className=" w-full">
+          {faqData.map((item, index) => (
+            <div
+              key={index}
+              className={`bg-white p-5 mb-4 ${
+                activeBorder === index ? "border-l-4 border-brand" : ""
+              }`}
+            >
+              <div
+                className="flex cursor-pointer items-center justify-between gap-10 "
+                onClick={() => handleClick(index)}
+              >
+                <h1 className="text-sm md:text-[18px]">{item.question}</h1>
+                <div
+                  className={`transform transition-transform ${
+                    activeItem === index
+                      ? "rotate-[-90deg] duration-500 bg-brand rounded-full text-white border-l-4 border-brand"
+                      : "bg-[#f3f8f6] rounded-full"
+                  }`}
+                >
+                  <MdKeyboardArrowDown size={25} />
+                </div>
+              </div>
+
+              <div
+                className={`transition-max-height mt-2 overflow-hidden ${
+                  activeItem === index
+                    ? "max-h-[1000px] duration-700 ease-in-out"
+                    : "max-h-0 duration-500 ease-in-out"
+                } ${
+                  activeBorder === index
+                    ? "duration-5000 ease-in  transition "
+                    : "duration-5000 ease-out transition "
+                }`}
+              >
+                <p>{item.answer}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
